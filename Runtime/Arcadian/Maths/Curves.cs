a420f7955a91e4782ce77c6b933e6605e04a3205
Lucas Warwick <lucaswarwick02@gmail.com>
2024-08-14 19:35:31 +0100

    Add Maths assets

---
+++ Runtime/Arcadian/Maths/Curves.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Arcadian.Maths
+{
+    /// <summary>
+    /// Used to store animation curves for use in animations.
+    /// </summary>
+    public static class Curves
+    {
+        /// <summary>
+        /// AnimationCurve that starts at 0 and ends at 1.
+        /// </summary>
+        public static readonly AnimationCurve In = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+        
+        /// <summary>
+        /// AnimationCurve that starts at 1 and ends at 0.
+        /// </summary>
+        public static readonly AnimationCurve Out = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
+    }
+}
