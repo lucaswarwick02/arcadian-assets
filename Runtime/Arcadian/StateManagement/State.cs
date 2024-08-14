53c1633b6bd8c9720b7c55a9db3f9bd0e51d6017
Lucas Warwick <lucaswarwick02@gmail.com>
2024-08-14 19:52:19 +0100

    Add StateManagement assets

---
+++ Runtime/Arcadian/StateManagement/State.cs
@@ -0,0 +1,9 @@
+﻿namespace Arcadian.StateManagement
+{
+    public abstract class State
+    {
+        public virtual void StartState() { }
+
+        public virtual void EndState() { }
+    }
+}
---
+++ Runtime/Arcadian/StateManagement/StateMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Arcadian.StateManagement
+{
+    public class StateMachine<T> where T : State
+    {
+        public T CurrentState { get; private set; }
+        
+        private T[] States { get; }
+        public event Action onStateChange;
+
+        public StateMachine(T[] states)
+        {
+            States = states;
+        }
+
+        public void SetState(Type type)
+        {
+            // No changes in state
+            if (CurrentState != null && type == CurrentState.GetType()) return;
+
+            CurrentState?.EndState();
+            
+            CurrentState = States.FirstOrDefault(state => state.GetType() == type);
+            
+            CurrentState?.StartState();
+            onStateChange?.Invoke();
+        }
+    }
+}
