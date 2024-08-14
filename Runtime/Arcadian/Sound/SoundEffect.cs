d3d04b4fd7064033642bc4639019aee4d04de52f
Lucas Warwick <lucaswarwick02@gmail.com>
2024-08-14 19:16:33 +0100

    Add Sound assets

---
+++ Runtime/Arcadian/Sound/SoundEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Arcadian.Sound
+{
+    [CreateAssetMenu(menuName = "Misc/Sound Effect", fileName = "New Sound Effect")]
+    public class SoundEffect : ScriptableObject
+    {
+        [field: SerializeField] public SoundEffectInstance Prefab { set; get; }
+        [field: SerializeField] public AudioClip Clip { private set; get; }
+        [field: SerializeField] public AudioMixerGroup MixerGroup { private set; get; }
+
+        public void Play(float? clipLength = null)
+        {
+            // Instantiate the standalone object
+            var audioSource = Instantiate(Prefab);
+
+            // Attach the Variables
+            audioSource.SetClip(Clip);
+            audioSource.SetMixerGroup(MixerGroup);
+            if (clipLength != null) audioSource.SetClipLength(clipLength.Value);
+
+            // Play
+            audioSource.Play();
+        }
+    }
+}
---
+++ Runtime/Arcadian/Sound/SoundEffectInstance.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Arcadian.Sound
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundEffectInstance : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void SetClip(AudioClip audioClip)
+        {
+            _audioSource.clip = audioClip;
+        }
+
+        public void SetMixerGroup(AudioMixerGroup audioMixerGroup)
+        {
+            _audioSource.outputAudioMixerGroup = audioMixerGroup;
+        }
+
+        public void SetClipLength(float targetLength)
+        {
+            _audioSource.pitch = _audioSource.clip.length / targetLength;
+        }
+
+        public void Play()
+        {
+            _audioSource.Play();
+
+            StartCoroutine(LifetimeCheck());
+        }
+
+        private IEnumerator LifetimeCheck()
+        {
+            while (_audioSource.isPlaying)
+            {
+                yield return null;
+            }
+            
+            // Clip has finished
+            Destroy(_audioSource.gameObject);
+        }
+    }
+}
