a14025fc0fa52bd05e089813558eed71523ba202
Lucas Warwick <lucaswarwick02@gmail.com>
2024-08-16 20:17:32 +0100

    Add AbstractUI

---
+++ Runtime/Arcadian/UI/AbstractUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using Arcadian.Sound;
+using UnityEngine;
+
+namespace Arcadian.UI
+{
+    public class AbstractUI : MonoBehaviour
+    {
+        [Header("Abstract UI")]
+        [SerializeField] private SoundEffect openSound;
+        [SerializeField] private SoundEffect closeSound;
+
+        private const float SizeMultiplier = 1.075f;
+        private const float AnimationLength = 0.125f;
+
+        private Coroutine _animation;
+    
+        public void Open()
+        {
+            gameObject.SetActive(true);
+            if (openSound) openSound.Play();
+            _animation = StartCoroutine(OpenAnimation());
+        }
+
+        private IEnumerator OpenAnimation()
+        {
+            var timer = 0f;
+
+            while (timer < AnimationLength)
+            {
+                timer += Time.deltaTime;
+                
+                var percentage = Mathf.Sin(timer / AnimationLength * Mathf.PI);
+                transform.localScale = Vector3.one * Mathf.Lerp(1f, SizeMultiplier, percentage);
+
+                yield return null;
+            }
+
+            transform.localScale = Vector3.one;
+        }
+    
+        public void Close()
+        {
+            if (closeSound) closeSound.Play();
+            if (_animation != null) StopCoroutine(_animation);
+            transform.localScale = Vector3.one;
+            gameObject.SetActive(false);
+        }
+    }
+}
