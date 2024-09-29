4bf2c77edc573d380028725c43205f7d4852b7c9
Lucas Warwick <lucaswarwick02@gmail.com>
2024-09-29 15:17:39 +0100

    Add FunctionalButton

---
+++ Runtime/Arcadian/UI/FunctionalButton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public abstract class FunctionalButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, ISelectHandler, IDeselectHandler, ISubmitHandler
+    {
+        public abstract void OnButtonHover();
+        public abstract void OnButtonHoverEnd();
+
+        public abstract void OnButtonPress();
+        
+        /// <summary>
+        /// Use this callback to detect pointer enter events
+        /// </summary>
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            OnButtonHover();
+        }
+
+        /// <summary>
+        /// Use this callback to detect pointer exit events
+        /// </summary>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            OnButtonHoverEnd();
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            OnButtonHover();
+        }
+
+        /// <summary>
+        /// Called by the EventSystem when a new object is being selected.
+        /// </summary>
+        public void OnDeselect(BaseEventData eventData)
+        {
+            OnButtonHoverEnd();
+        }
+
+        /// <summary>
+        /// Use this callback to detect clicks.
+        /// </summary>
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnButtonPress();
+        }
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            OnButtonPress();
+        }
+    }
+}
